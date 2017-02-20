588eee11e115b1e213f53eaf6c0999974df96180
meepingsnesroms <guicrith@gmail.com>
2017-02-20 11:26:29 -0800

    Put back zlib, add -lz flag for mac

--- libretro/include/config.h
+++ libretro/include/config.h
@@ -133,7 +133,7 @@
 #define HAVE_UNISTD_H 1
 
 /* Can we use the ZLIB compression library? */
-//#define HAVE_ZLIB
+#define HAVE_ZLIB 1
 
 /* Define to the full name of this package. */
 #define PACKAGE "vice"

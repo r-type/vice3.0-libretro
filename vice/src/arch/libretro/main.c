90b8ed8369508974b0b5081b14de43c38ca7f126
twinaphex <libretro@gmail.com>
2017-03-24 14:02:33 +0100

    Assume no libco by default

--- vice/src/arch/libretro/main.c
+++ vice/src/arch/libretro/main.c
@@ -7,19 +7,14 @@
 
 int skel_main(int argc, char *argv[])
 {
+   main_program(argc, argv);
 
-  main_program(argc, argv);
-//  main_exit();
-#ifndef NO_LIBCO
-  vice_main_exit();
-#endif
- 
-  return(0);
+   return(0);
 }
 
 void vice_main_exit()
 {
-printf("vice exit\n");
-    machine_shutdown();
+   printf("vice exit\n");
+   machine_shutdown();
 }
 

b13d4c60d1496ff1b2c24c2cd4203a839d8bd58e
Tony Jansson <tony@pihaus.org>
2020-09-08 17:49:44 +0300

    Detach device 9 on unload if needed

--- libretro/libretro-core.c
+++ libretro/libretro-core.c
@@ -5442,6 +5442,8 @@ bool retro_load_game(const struct retro_game_info *info)
 void retro_unload_game(void)
 {
    file_system_detach_disk(8);
+   if (opt_work_disk_type && opt_work_disk_unit == 9)
+      file_system_detach_disk(9);
    tape_image_detach(1);
    cartridge_detach_image(-1);
    dc_reset(dc);

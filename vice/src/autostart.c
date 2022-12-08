a4e51e8fb62985995f3bae47fd2df034a6871660
Jamiras <32680403+Jamiras@users.noreply.github.com>
2022-12-08 13:15:34 -0700

    fix autostart not working on reset (#483)

--- vice/src/autostart.c
+++ vice/src/autostart.c
@@ -1715,6 +1715,11 @@ int autostart_disk(int unit, int drive, const char *file_name, const char *progr
 
     if (name) {
         autostart_disk_cook_name(&name);
+
+#ifdef __LIBRETRO__
+        file_system_detach_disk(unit, drive);
+#endif
+
         if (!(file_system_attach_disk(unit, drive, file_name) < 0)) {
 #if 1
             vdrive_t *vdrive;
